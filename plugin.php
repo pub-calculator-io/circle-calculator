d8f23e2852103138d38a8ccd28edb7af13769ef7
Arseniy Olevskiy <arsidian@gmail.com>
2023-10-24 16:06:32 +0400

    regular commit

--- plugin.php
+++ plugin.php
@@ -10,6 +10,8 @@ License: GPLv2 or later
 Text Domain: ci_circle_calculator
 */
 
+if (!defined('ABSPATH')) exit;
+
 if (!function_exists('add_shortcode')) return "No direct call for Circle Calculator by Calculator.iO";
 
 function display_ci_circle_calculator(){
